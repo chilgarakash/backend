6458d13da827ccc65721de6795787a5923165ec3
Akash <akashchilgar05@gmail.com>
2024-05-06 20:19:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+    res.send('Hello Akash!')
+})
+app.get('/login', (req, res) => {
+    res.send('<h1>Please Login to Sunsky Page</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
